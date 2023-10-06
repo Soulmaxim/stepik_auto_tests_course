7bda8c29143d6ad9d38c172cdd9cf1d9ae218623
Meks <soulmaxim@gmail.com>
2023-10-06 09:56:48 +0300

    new scripts

---
+++ selenium_course/script_3_2_10.py
@@ -0,0 +1,39 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+
+import time
+import math
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+def passCapcha(browser):
+    x = browser.find_element(By.ID, 'input_value').text
+    solve = calc(x)
+    answer_input = browser.find_element(By.ID, 'answer')
+    answer_input.send_keys(solve)
+    submit_button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+    submit_button.click()
+
+try:
+    link = 'http://suninjuly.github.io/explicit_wait2.html'
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    WebDriverWait(browser, 15).until(expected_conditions.text_to_be_present_in_element((By.ID, 'price'), '100'))
+    button = browser.find_element(By.ID, 'book')
+    button.click()
+
+    x = browser.find_element(By.ID, 'input_value').text
+    solve = calc(x)
+    answer_input = browser.find_element(By.ID, 'answer')
+    answer_input.send_keys(solve)
+    submit_button = browser.find_element(By.ID, "solve")
+    submit_button.click()
+
+finally:
+    time.sleep(7)
+    browser.quit()
+
